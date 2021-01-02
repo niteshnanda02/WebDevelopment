c2e5c665e0879149294d1b10a1d42b8876f688cf
Nitesh <niteshnanda02>
2021-01-03 01:17:15 +0530

    Using Passport.js to add cookies and Sessions

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -5,27 +5,41 @@ const express= require("express");
 const ejs=require("ejs");
 const bodyParser = require("body-parser");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
-const bcrypt=require("bcrypt");
-const saltRounds=10;
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 
 const app=express();
-
-mongoose.connect("mongodb://localhost:27017:/userDB",{ useNewUrlParser: true , useUnifiedTopology: true });
-
-const userSchema= new mongoose.Schema({
-    email: String,
-    password: String
-});
-
-
-const User=new mongoose.model("User",userSchema);
 app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017:/userDB",{ useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.set("useCreateIndex",true);
+const userSchema= new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User=new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.get("/",function(req,res){
     res.render("home");
 });
@@ -34,49 +48,54 @@ app.route("/login")
         res.render("login");
     })
     .post(function(req,res){
-        const username=req.body.username;
-        const password=req.body.password;
+        const user=new User({
+            username: req.body.username,
+            password: req.body.password
+        });
 
-        User.findOne({email: username},function(err,foundUser){
+        req.logIn(user, function(err){
             if(err)
                 console.log(err);
             else{
-                if(foundUser){
-                    bcrypt.compare(password, foundUser.password, function(err, result) {
-                        // result == true
-                        if(result==true){
-                            res.render("secrets");
-                        }
-                    });
-                }
-                
+                passport.authenticate("local")(req, res, function(){
+                    res.redirect("/secrets");
+                })
             }
-        });
-    })
+        })
+    });
 app.route("/register")
     .get(function(req,res){
         res.render("register");
     })
     .post(function(req,res){
-        bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-
-            const newUser=new User({
-                email: req.body.username,
-                password: hash
-            });
-            newUser.save(function(err){
-                if(err){
-                    console.log(err);
-                }else{
-                    res.render("secrets");
-                }
-            });
+        User.register({username: req.body.username}, req.body.password, function(err,user){
+            if(err){
+                console.log(err);
+                res.redirect("/register");
+            }else{
+                passport.authenticate("local")(req, res, function(){
+                    res.redirect("/secrets");
+                });
+            }
         });
-
        
     });
 
+app.route("/secrets")
+    .get(function(req,res){
+        if(req.isAuthenticated()){
+            res.render("secrets");
+        }else{
+            res.redirect("/login");
+        }
+    });
 
+app.route("/logout")
+    .get(function(req,res){
+        req.logOut();
+        res.redirect("/");
+    });
+    
 app.listen(3000,function(){
     console.log("Server started at port 3000");
 });
